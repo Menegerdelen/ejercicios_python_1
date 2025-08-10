bd22a34b23ea9c9c3468c590aeb0cf9b1afda71c
Manuel Nuñez <menegerdelen@gmail.com>
2025-08-09 22:42:59 -0400

    Ejercicio 1

---
+++ e01_ejercicios_strings/separar_palabras.py
@@ -0,0 +1,9 @@
+# Escribe un programa que pida al usuario que ingrese 3 palabras y las muestre por consola separadas por un guion
+
+print("Hola, ingrese sus tres palabras: ")
+
+word1 = input("Palabra uno: \n")
+word2 = input("Palabra 2: \n")
+word3 = input("Palabra 3: \n")
+
+print(f"El listado de palabras es: {word1} - {word2} - {word3}")
